de3ded6ccf54f683bdd3fa6ffdf731b4c69a5559
Christopher Jones <tkcwebdev@gmail.com>
2020-12-24 03:34:15 -0800

    Comment Routes file to keep everything clean

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// ...Routes all go here!
+
+// Always check to see our server is running and healthy
 router.get('/healthcheck', (req, res) => {
 	try {
 		res.send({ status: 'Everything is healthy BUD' });
