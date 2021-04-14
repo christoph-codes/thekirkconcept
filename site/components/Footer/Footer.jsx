781d75eb53b158c79c935c89e246df74cd1c5af9
Christopher Jones <tkcwebdev@gmail.com>
2021-04-13 20:08:12 -0700

    Homepage progress. Working on Footer

--- site/components/Footer/Footer.jsx
+++ site/components/Footer/Footer.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Section from '../Section';
+import {
+	FaHeart,
+	FaFacebook,
+	FaInstagram,
+	FaLinkedin,
+	FaMedium,
+	FaTwitter,
+	FaYoutube,
+} from 'react-icons/fa';
 import './Footer.module.scss';
+import Link from 'next/link';
 
 const Footer = (props) => {
-	return <div className='Footer'>Footer</div>;
+	return (
+		<Row as={Section} className='Footer align-items-center'>
+			<Col>
+				<Link href='https://facebook.com/thekirkconcept'>
+					<a className='mr-10'>
+						<FaFacebook color='#454545' size={18} />
+					</a>
+				</Link>
+				<Link href='https://twitter.com/thekirkconcept'>
+					<a className='mr-10'>
+						<FaTwitter color='#454545' size={18} />
+					</a>
+				</Link>
+				<Link href='https://instagram.com/thekirkconcept'>
+					<a className='mr-10'>
+						<FaInstagram color='#454545' size={18} />
+					</a>
+				</Link>
+				<Link href='https://medium.com/thekirkconcept'>
+					<a className='mr-10'>
+						<FaMedium color='#454545' size={18} />
+					</a>
+				</Link>
+				<Link href='https://youtube.com/thekirkconcept'>
+					<a className='mr-10'>
+						<FaYoutube color='#454545' size={18} />
+					</a>
+				</Link>
+				<Link href='https://linkedin.com/thekirkconcept'>
+					<a className='mr-10'>
+						<FaLinkedin color='#454545' size={18} />
+					</a>
+				</Link>
+			</Col>
+			<Col className='text-right'>
+				Site made with <FaHeart color='#e2ff08' /> copyright © 2021 The
+				Kirk Concept. All rights reserved.{' '}
+			</Col>
+		</Row>
+	);
 };
 
 export default Footer;
--- site/pages/index.jsx
+++ site/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import {Row,Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import Button from '../components/Button/Button';
 import Section from '../components/Section';
 import SubHero from '../components/SubHero/SubHero';
@@ -15,7 +15,10 @@ export default function Home() {
 				<title>The Kirk Concept</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<Section height="90vh" className='home-hero bg-gray text-center align-items-center'>
+			<Section
+				height='90vh'
+				className='home-hero bg-gray text-center align-items-center'
+			>
 				<h1 className='fs-xl font-weight-bold text-white-50'>
 					We unlock brand
 					<br />
@@ -34,27 +37,92 @@ export default function Home() {
 				</Link>
 			</Section>
 			<SubHero tagline='We have systems in place to birth excellence.'>
-					<Col className="p-30 shadow bg-white rounded-lg text-gray">
-						<h3 className="fs-sm font-weight-bold mb-20">Branding</h3>
-						<p>Brand dominance is only achieved with a clear and purposeful brand strategy and visual connection that resonates with your audience and industry.</p>
-						<Link href="/branding">
-						<a className="text-gray primary-underline">Learn More</a></Link>
-					</Col>
-					<Col className="p-30 shadow bg-white rounded-lg text-gray">
-						<h3 className="fs-sm font-weight-bold mb-20">Design System</h3>
-						<p>A dominant design systems creates consistent experiences across multiple mediums and platforms to establish brand equity on the digital level.</p>
-						<Link href="/design-system">
-						<a className="text-gray primary-underline">Learn More</a></Link>
-					</Col>
-					<Col className="p-30 shadow bg-white rounded-lg text-gray">
-						<h3 className="fs-sm font-weight-bold mb-20">App Development</h3>
-						<p>Mobile and web application development brings a dominant brand to life through user experiences that connect a product and a loyal customer.</p>
-						<Link href="/development">
-						<a className="text-gray primary-underline">Learn More</a></Link>
-					</Col>
+				<Col className='p-30 shadow bg-white rounded-lg text-gray'>
+					<h3 className='fs-sm font-weight-bold mb-20'>Branding</h3>
+					<p>
+						Brand dominance is only achieved with a clear and
+						purposeful brand strategy and visual connection that
+						resonates with your audience and industry.
+					</p>
+					<Link href='/branding'>
+						<a className='text-gray primary-underline'>
+							Learn More
+						</a>
+					</Link>
+				</Col>
+				<Col className='p-30 shadow bg-white rounded-lg text-gray'>
+					<h3 className='fs-sm font-weight-bold mb-20'>
+						Design System
+					</h3>
+					<p>
+						A dominant design systems creates consistent experiences
+						across multiple mediums and platforms to establish brand
+						equity on the digital level.
+					</p>
+					<Link href='/design-system'>
+						<a className='text-gray primary-underline'>
+							Learn More
+						</a>
+					</Link>
+				</Col>
+				<Col className='p-30 shadow bg-white rounded-lg text-gray'>
+					<h3 className='fs-sm font-weight-bold mb-20'>
+						App Development
+					</h3>
+					<p>
+						Mobile and web application development brings a dominant
+						brand to life through user experiences that connect a
+						product and a loyal customer.
+					</p>
+					<Link href='/development'>
+						<a className='text-gray primary-underline'>
+							Learn More
+						</a>
+					</Link>
+				</Col>
 			</SubHero>
-			<Section fullWidth className="py-50" containerClass="px-0">
-				<WordRiver words={['adobe illustrator', 'reactjs', 'logos', 'consistency', 'docker', 'vscode', 'branding', 'ios', 'systems', 'custom', 'sketches', 'git', 'button', 'mobile app', 'efficiency', 'storybook', 'api', 'icons', 'dashboard', 'grid', 'hooks', 'strategy', 'launch', 'messaging']} colors={['#e6fd36','#454545','#A5A5A5']}/>
+			<Section fullWidth className='py-50' containerClass='px-0'>
+				<WordRiver
+					words={[
+						'adobe illustrator',
+						'reactjs',
+						'logos',
+						'consistency',
+						'docker',
+						'vscode',
+						'branding',
+						'ios',
+						'systems',
+						'custom',
+						'sketches',
+						'git',
+						'button',
+						'mobile app',
+						'efficiency',
+						'storybook',
+						'api',
+						'icons',
+						'dashboard',
+						'grid',
+						'hooks',
+						'strategy',
+						'launch',
+						'messaging',
+					]}
+					colors={['#e6fd36', '#454545', '#A5A5A5']}
+				/>
+			</Section>
+			<Section
+				height='15rem'
+				containerClass='font-display'
+				className='text-center font-weight-light'
+			>
+				<hr className='primary small-width' />
+				<p className='fw-light fs-sm mw-lg'>
+					TKC exists to establish industry visual dominance for SMB’s
+					through impeccable branding, cohesive design systems, and
+					intentional web and mobile app experiences.
+				</p>
 			</Section>
 		</Page>
 	);
