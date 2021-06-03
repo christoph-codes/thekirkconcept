3b11f86634d13c8108b67cd37e7605218547408a
Christopher Jones <tkcwebdev@gmail.com>
2021-06-03 01:51:20 -0700

    Fixed case study syntax for pff which broke the build

--- site/pages/case-study/pff.jsx
+++ site/pages/case-study/pff.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CaseStudy from '../../templates/CaseStudy';
-import pff from '../../utilities/caseStudies/PFF';
+import pff from '../../utilities/caseStudies/pff';
 
 const CaseStudyPFF = () => {
 	return <CaseStudy caseStudy={pff} className="CaseStudyPFF" />;
