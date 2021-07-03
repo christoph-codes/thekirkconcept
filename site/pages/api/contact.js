aceef9ca75482629d7f309db7d770b5d4fdf0793
Christopher Jones <tkcwebdev@gmail.com>
2021-07-02 23:42:19 -0700

    Add try catch to contact function to ensure api call doesn't fail

--- site/pages/api/contact.js
+++ site/pages/api/contact.js
@@ -5,22 +5,32 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const contact = async (req, res) => {
 	const { emailDetails } = req.body;
-
-	const msg = {
-		to: 'hello@thekirkconcept.com', // Change to your recipient
-		from: 'hello@thekirkconcept.com', // Change to your verified sender
-		subject: 'Contact Submission from TKCWEB',
-		text: 'Make sure to reach out because you know you care!',
-		html: ContactTemplate(emailDetails),
-	};
-	await sgMail
-		.send(msg)
-		.then(() => {
-			console.log('Email sent');
-			res.json('everything sent');
-		})
-		.catch((error) => {
-			console.error(error);
+	if (emailDetails) {
+		try {
+			const msg = {
+				to: 'hello@thekirkconcept.com', // Change to your recipient
+				from: 'hello@thekirkconcept.com', // Change to your verified sender
+				subject: 'Contact Submission from TKCWEB',
+				text: 'Make sure to reach out because you know you care!',
+				html: ContactTemplate(emailDetails),
+			};
+			await sgMail
+				.send(msg)
+				.then(() => {
+					console.log('Email sent');
+					res.json('everything sent');
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+		} catch (e) {
+			console.log(e);
+			res.send({ status: e });
+		}
+	} else {
+		res.send({
+			error: 'All of the details for this email has not been submitted.',
 		});
+	}
 };
 export default contact;
